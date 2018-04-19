65006d06e7e1aa3767e91c5275f84cf3e7d64a45
rubyist <rubyist.personal@openmailbox.org>
2018-04-18 18:30:51 -0700

    Added selfish nodes and network topology. Fixed lagg spikes from all_simple_paths using a cutoff.

--- baseline-manet.py
+++ baseline-manet.py
@@ -36,12 +36,12 @@ class Message:
 # Every node in the network has an index and an associated device.
 # The index is how the device is found in the global NETWORK object.
 # The device is an actor that creates, receives, and acts upon messages.
+# Every device has a set level of "greed", indicating how likely it is to behave
+# selfishly. A value of 0 means never, 1 means always, in between is random.
 class Device:
-    # TODO: consider a "selfishness score" to show how likely a node is to be
-    # selfish. A score of 1 is always selfish, 0 is never selfish. In between is
-    # a perhaps random choice. (DISCUSS?)
-    def __init__(self, index):
+    def __init__(self, index, greed):
         self.index = index
+        self.greed = greed
     
     # Forwards a message to the best receiver possible.
     # In other implementations, this is where we would place our "trust" system.
@@ -56,9 +56,10 @@ class Device:
             return msg.dst
     
         # Find a path that does not return to a previously visited node.
-        paths = nx.all_simple_paths(NETWORK, self.index, msg.dst)
-        options = [p for p in paths if not msg.has_cycles_in(p)]
-    
+        spl = nx.shortest_path_length(NETWORK, self.index, msg.dst)
+        paths = nx.all_simple_paths(NETWORK, self.index, msg.dst, cutoff=3+spl)
+        options = [p for p in list(paths) if not msg.has_cycles_in(p)]
+
         # If none exist, drop the packet. This should not happen.
         if len(options) == 0:
             return None
@@ -82,8 +83,10 @@ class Device:
         # trust scores in our "trusty" implementation.
         if msg.dst == self.index:
             print("Received a packet:", msg.content)
-        else:
+        elif random.random() >= self.greed:
             self.forward_msg(msg)
+        else:
+            print("Packet eaten by black hole")
     
     # Create a new message to a random destination, and send it off.
     def produce_msg(self):
@@ -96,10 +99,25 @@ class Device:
         msg = Message(src, dst, "a")
         self.forward_msg(msg)
 
-# Example of how a network might be set up.
-NETWORK.add_node(1, device=Device(1))
-NETWORK.add_node(2, device=Device(2))
-NETWORK.add_edge(1, 2)
+# Setup network connections
+black_holes = [1,2,8,11,22]
+for i in range(0, 26):
+    greed = float(i in black_holes)
+    NETWORK.add_node(i, device=Device(i, greed))
+
+NETWORK.add_edges_from([
+    ( 0,  1), ( 0,  2), ( 0,  4), ( 0, 14), ( 0, 16), ( 0, 21),
+    ( 1,  2), ( 1,  3), ( 1,  4), ( 1,  5), ( 1, 15), ( 1, 16),
+    ( 2,  4), ( 2, 12), ( 2, 14), ( 2, 21), ( 3,  5), ( 3, 13),
+    ( 3, 15), ( 3, 17), ( 4,  6), ( 4, 12), ( 5,  7), ( 5, 11),
+    ( 5, 13), ( 6,  8), ( 6, 10), ( 6, 18), ( 7,  9), ( 7, 11),
+    ( 8, 10), ( 8, 18), ( 9, 11), ( 9, 19), ( 9, 22), ( 9, 25),
+    (10, 12), (11, 13), (11, 18), (11, 19), (11, 24), (12, 14),
+    (13, 15), (13, 17), (13, 18), (14, 21), (15, 16), (15, 17),
+    (16, 21), (17, 18), (18, 19), (18, 24), (19, 20), (19, 22),
+    (19, 24), (20, 22), (20, 23), (22, 23), (22, 25), (23, 25)
+])
+
 
 # Example of how a message might be sent.
 # Note that we will need a proper benchmark, perhaps by iterating over every
